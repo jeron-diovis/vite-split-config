f6319c4e45e654b4e8620943c3bdb94902fd4f31
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2023-02-24 13:17:35 +0200

    add vitest

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,17 @@
-import * as X from './index'
+import { mergeConfig } from './index'
 
-describe('base', () => {
-  it('should work', () => {
-    expect(2).toBe(2)
+describe('mergeConfig', () => {
+  it('should recursively merge objects', () => {
+    const merged = mergeConfig({ a: { b: 1, c: 2 } }, { a: { c: 3, d: 4 } })
+    expect(merged).toStrictEqual({
+      a: { b: 1, c: 3, d: 4 },
+    })
+  })
+
+  it('should concat arrays', () => {
+    const merged = mergeConfig({ a: { b: [1, 2] } }, { a: { b: [3, 4] } })
+    expect(merged).toStrictEqual({
+      a: { b: [1, 2, 3, 4] },
+    })
   })
 })
